c98abebe397c5c2bc917c897075ef95d1dee92e2
JulianHysi <julian_hysi@yahoo.com>
2022-06-09 14:03:21 +0200

    Complete sections about mutability and classes

--- session1/4_mutability.py
+++ session1/4_mutability.py
@@ -3,3 +3,74 @@ Video Tutorial: https://www.youtube.com/watch?v=p9ppfvHv2Us
 common mistakes with default values
 tuple vs list
 """
+
+# strings are immutable
+name = "John Doe"
+print(name[0])
+name[0] = 'H'  # raises TypeError
+
+
+# lists are mutable
+colors = ['blue', 'green', 'red', 'yellow']
+colors.pop()
+print(colors)
+new_colors = colors
+new_colors.pop()
+print(new_colors)
+print(colors)
+
+
+# tuples are immutable
+colors = ('blue', 'green', 'red', 'yellow')
+new_colors = colors
+del new_colors
+print(colors)  # the original tuple is not affected
+
+
+# sets are mutable
+colors = {'blue', 'green', 'red', 'yellow'}
+colors.pop()
+print(colors)
+
+
+# dicts are mutable
+person = {
+    'height': 1.60,
+    'weight': '50kg',
+    'age': 21
+}
+person.pop('weight')
+print(person)
+
+
+# for mutable data structures, use the copy() when you want a copy of the original,
+# but you do not want the changes to be reflected
+list_a = [1, 2, 3]
+list_b = list_a.copy()
+list_b.pop()
+print(list_a)
+
+
+# Mutable defaults (anti pattern)
+def create_or_append(value, list_=[]):
+    list_.append(value)
+    return list_
+
+
+colors = ['blue', 'green', 'red', 'yellow']
+new_list = create_or_append('purple', colors)
+print(new_list)
+
+# but the problem with defaults starts when calling it without the default arg
+second_list = create_or_append('grey')
+print(second_list)  # list with one element, as expected
+third_list = create_or_append('orange')
+print(third_list)
+
+
+# the solution
+def create_or_append(value, list_=None):
+    if list_ is None:
+        list_ = []
+    list_.append(value)
+    return list_
--- session1/6_oop/1_classes.py
+++ session1/6_oop/1_classes.py
@@ -6,3 +6,42 @@ class attributes, instance attributes, constants
 
 Inheritance vs composition when to choose which one?
 """
+
+
+class User:
+    pass
+
+
+obj_1 = User()
+print(type(obj_1))
+
+
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def get_username(self):  # Note: dont use getters and setters in Python!
+        return self.username
+
+
+admin = User('admin001', '123456', 'admin001@mysite.com')
+print(admin.email)
+print(admin.get_username())
+admin.username = 'admin002'  # you can change the value of an object property even after creation
+print(admin.get_username())
+del admin.username
+print(admin.get_username())  # you can delete the property altogether
+
+try:
+    admin.get_username()
+except AttributeError:
+    print("Property does not exist")
+
+del admin  # de-reference an object
+
+try:
+    admin.get_username()
+except NameError:
+    print("No object of such name exists")
